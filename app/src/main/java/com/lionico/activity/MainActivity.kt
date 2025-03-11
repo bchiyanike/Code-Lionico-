e4e3e8ee9c269cb3ba612e4c4b31477bb40e209a
bchiyanike <bchiyanike@gmail.com>
2025-03-11 15:22:06 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/lionico/activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.lionico.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.lionico.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            .setupWithNavController(navController)
+    }
+}
